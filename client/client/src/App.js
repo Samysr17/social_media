22d7722346b58e60075af4f51f85646676d8da6c
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-12-19 18:45:22 +0530

    frontend skeleton

---
+++ client/client/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter,Route,Routes,Navigate} from "react-router-dom";
+import Home from "./Home";
+import profile from "./profile";
+import Login from "./Login";
+function App() {
+  return (
+    <div className="app">
+     <BrowserRouter>
+     <Routes>
+      
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
