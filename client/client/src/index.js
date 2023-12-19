343371df89621575249e5f36c6f2464cfe4854ef
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-12-19 23:37:45 +0530

    bug fixing

--- client/client/src/index.js
+++ client/client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import authReducer from './state';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
 import {
   persistStore,
   persistReducer,
@@ -18,10 +19,28 @@ import {
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
+const persistConfig={key:"root",storage,version:1}
+const persistedReducer=persistReducer(persistConfig,authReducer);
+const store=configureStore({
+  reducer:persistedReducer,
+  middleware:(getDefaultMiddleware)=>
+  getDefaultMiddleware({
+    serializableCheck:{
+      ignoredActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER],
+    }
+  }),
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+   <React.StrictMode>
+  {/* //   <Provider store={store}>
+  //     <PersistGate loading={null} persistor={persistStore(store)}> */}
     <App />
+    {/* </PersistGate>
+    </Provider> */}
   </React.StrictMode>
 );
+
+
 
