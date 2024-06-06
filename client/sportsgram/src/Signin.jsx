86fde95cbf394d82a2c7181533364c020b9d5d1a
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2024-06-06 13:14:49 +0530

    frontend backend link

--- client/sportsgram/src/Signin.jsx
+++ client/sportsgram/src/Signin.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 import Football from './assets/Football.jpg'
-
+import {useDropzone} from 'react-dropzone'
+import { useCallback } from 'react';
+import { FaPlusCircle } from "react-icons/fa";
 const Signin = () => {
+  const [name,setname]=useState('');
+  const [email,setemail]=useState('');
+  const [password,setpassword]=useState('');
+  const [image,setimage]=useState([]);
+  const [select,setselect]=useState(false);
+  const onDrop = useCallback(acceptedFiles => {
+    setimage(acceptedFiles.map(file=>
+      Object.assign(file,{
+        name:file.Name,
+        source:URL.createObjectURL(file)
+      })
+       
+    ))
+    setselect(true);
+    console.log(image[0].name);
+  }, [])
+  const {getRootProps, getInputProps,isDragActive} = useDropzone({onDrop});
+  const handleclick=()=>{
+   
+  }
   return (
     <>
      <div className='w-full h-screen'>
@@ -21,24 +44,44 @@ const Signin = () => {
                 className='w-full flex flex-col py-4'
               >
                 
-                <input  className='p-3 my-2 bg-white rounded' type='email'
+                <input  className='p-3 my-2 bg-white rounded text-black' 
+                  onChange={(e)=>setname(e.target.value)}
+                  required
                   placeholder='Name'
                   autoComplete='Name' />
-                <input  className='p-3 my-2 bg-white rounded' type='email'
+                <input  className='p-3 my-2 bg-white rounded text-black' type='email'
+                  onChange={(e)=>setemail(e.target.value)}
+                  required
                   placeholder='Email'
                   autoComplete='email' />
                   {/* {error ?<p className='text-red'>{error}</p>:null} */}
                 <input
-                  className='p-3 my-2 bg-white-300 rounded'
+                  className='p-3 my-2 bg-white-300 rounded text-black'
+                  onChange={(e)=>setpassword(e.target.value)}
+                  required
                   type='password'
                   placeholder='Password'
                   autoComplete='current-password'
                 />
-                  <input  className='p-3 my-2 bg-white rounded' 
-                //   type='image'
-                //   alt="/"
-                  placeholder='Image'/>
-                <button  className='bg-blue-700 py-3 my-6 rounded font-bold text-white'>
+          <div  className="p-3 my-2 bg-white rounded" {...getRootProps()}>
+          <input type='image' alt="/"  required  {...getInputProps()} />
+       <div >
+       {
+        isDragActive ?
+          <p className="text-black">Drop the files here ...</p> :(
+            <div className="flex justify-between">
+          <p className="text-gray-400">Profile Picture</p>
+      <FaPlusCircle size={20} className='text-gray-400'/>
+          </div>
+          
+          )
+      }
+      <div className="flex justify-center space-x-4">
+       {select?<img className="h-[100px] " src={image[0].source} alt="/"></img>:<div></div>}
+       </div>
+        </div>
+           </div>
+                <button onSubmit={handleclick}  className='bg-blue-700 py-3 my-6 rounded font-bold text-white'>
                   Sign Up
                 </button>
                 <div className='flex justify-between items-center text-sm text-white-600'>
